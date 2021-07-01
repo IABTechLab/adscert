af54dccae18d03936521cefb29ae7d971351c1e6
Mani Gandham <manigandham@gmail.com>
2021-07-01 16:12:18 -0700

    domain indexer handles lookup existence and error differently

--- pkg/adscert/discovery/domain_indexer_impl.go
+++ pkg/adscert/discovery/domain_indexer_impl.go
@@ -13,7 +13,7 @@ import (
 
 func NewDefaultDomainIndexer(dnsResolver DNSResolver, domainStore DomainStore, base64PrivateKeys []string) DomainIndexer {
 
-	cm := &defaultDomainIndexer{
+	di := &defaultDomainIndexer{
 		ticker:      time.NewTicker(30 * time.Second),
 		wakeUp:      make(chan struct{}, 1),
 		dnsResolver: dnsResolver,
@@ -24,19 +24,19 @@ func NewDefaultDomainIndexer(dnsResolver DNSResolver, domainStore DomainStore, b
 	if err != nil {
 		logger.Fatalf("Error parsing private keys: %v", err)
 	}
-	cm.myPrivateKeys = myPrivateKeys
+	di.myPrivateKeys = myPrivateKeys
 
-	for _, privateKey := range cm.myPrivateKeys {
+	for _, privateKey := range di.myPrivateKeys {
 		// since iterating over a map is non-deterministic, we can make sure to set the key
 		// either if it is not already set or it is alphabetically less than current key at the index when
 		// iterating over the private keys map.
-		if cm.currentPrivateKey == "" || cm.currentPrivateKey < privateKey.alias {
-			cm.currentPrivateKey = privateKey.alias
+		if di.currentPrivateKey == "" || di.currentPrivateKey < privateKey.alias {
+			di.currentPrivateKey = privateKey.alias
 		}
 	}
 
-	cm.startAutoUpdate()
-	return cm
+	di.startAutoUpdate()
+	return di
 }
 
 type defaultDomainIndexer struct {
@@ -51,11 +51,21 @@ type defaultDomainIndexer struct {
 	domainStore DomainStore
 }
 
-func (cm *defaultDomainIndexer) LookupIdentitiesForDomain(invokingDomain string) ([]DomainInfo, error) {
+func (di *defaultDomainIndexer) LookupIdentitiesForDomain(invokingDomain string) ([]DomainInfo, error) {
 
-	domainInfo, err := cm.domainStore.LookupDomainInfo(context.Background(), invokingDomain)
+	domainInfo, ok, err := di.domainStore.LookupDomainInfo(context.Background(), invokingDomain)
 	if err != nil {
+		// lookup operation had a problem (depends on domain store implementation, for example network failure)
+		// return empty list
 		return []DomainInfo{}, err
+	}
+	if !ok {
+		// domain was not found in domain store
+		// store a new entry so it can be processed and queue an update
+		di.domainStore.StoreDomainInfo(context.Background(), DomainInfo{Domain: invokingDomain, lastUpdateTime: time.Now().UTC().Add(-1 * time.Hour)})
+		di.UpdateNow()
+		// return empty list
+		return []DomainInfo{}, nil
 	}
 
 	// if this domain contains public keys then its an identity domain
@@ -67,59 +77,56 @@ func (cm *defaultDomainIndexer) LookupIdentitiesForDomain(invokingDomain string)
 	// this domain is an invoking domain with (one or many) parent identity domains
 	// loop through and request domain info for each identity
 	if len(domainInfo.IdentityDomains) > 0 {
-		var info []DomainInfo
+		var domains []DomainInfo
 		for _, d := range domainInfo.IdentityDomains {
-			if di, err := cm.domainStore.LookupDomainInfo(context.Background(), d); err == nil {
-				info = append(info, di)
+			if info, ok, err := di.domainStore.LookupDomainInfo(context.Background(), d); err == nil && ok {
+				domains = append(domains, info)
 			}
 		}
 
-		return info, nil
+		return domains, nil
 	}
 
 	return nil, errors.New("failed to lookup identity domains for invoking domain")
 }
 
-func (cm *defaultDomainIndexer) startAutoUpdate() {
+func (di *defaultDomainIndexer) startAutoUpdate() {
 	var ctx context.Context
-	ctx, cm.cancel = context.WithCancel(context.Background())
+	ctx, di.cancel = context.WithCancel(context.Background())
 	go func() {
 		for {
 			select {
 			case <-ctx.Done():
 				logger.Info("shutting down auto-update")
 				return
-			case <-cm.ticker.C:
+			case <-di.ticker.C:
 				logger.Info("automatic wake-up")
-			case <-cm.wakeUp:
+			case <-di.wakeUp:
 				logger.Info("manual wake-up from wake-up signal")
 			}
-			cm.performUpdateSweep(ctx)
+			di.performUpdateSweep(ctx)
 		}
 	}()
 }
 
-func (cm *defaultDomainIndexer) performUpdateSweep(ctx context.Context) {
+func (di *defaultDomainIndexer) performUpdateSweep(ctx context.Context) {
 
 	logger.Info("Starting ads.cert update sweep")
-	domains, err := cm.domainStore.GetAllDomains(ctx)
+	domains, err := di.domainStore.GetAllDomains(ctx)
 	if err != nil {
 		logger.Warningf("Error retriving list of domains: %v", err)
 	}
 
 	for _, domain := range domains {
-
-		currentDomainInfo, err := cm.domainStore.LookupDomainInfo(ctx, domain)
+		currentDomainInfo, _, err := di.domainStore.LookupDomainInfo(ctx, domain)
 		if err != nil {
 			logger.Infof("unable to retrieve domain info for domain %s, skipping update until next loop", domain)
 
 		} else if currentDomainInfo.lastUpdateTime.Before(time.Now().Add(-300 * time.Second)) {
 			logger.Infof("Trying to do an update for domain %s", domain)
-
-			cm.checkDomainForPolicyRecords(ctx, &currentDomainInfo)
-			cm.checkDomainForKeyRecords(ctx, &currentDomainInfo)
-
-			cm.domainStore.StoreDomainInfo(ctx, currentDomainInfo)
+			di.checkDomainForPolicyRecords(ctx, &currentDomainInfo)
+			di.checkDomainForKeyRecords(ctx, &currentDomainInfo)
+			di.domainStore.StoreDomainInfo(ctx, currentDomainInfo)
 
 		} else {
 			logger.Infof("skipping update for domain %s which is already up to date.", domain)
@@ -127,11 +134,11 @@ func (cm *defaultDomainIndexer) performUpdateSweep(ctx context.Context) {
 	}
 }
 
-func (cm *defaultDomainIndexer) checkDomainForPolicyRecords(ctx context.Context, currentDomainInfo *DomainInfo) {
+func (di *defaultDomainIndexer) checkDomainForPolicyRecords(ctx context.Context, currentDomainInfo *DomainInfo) {
 
 	startTime := time.Now()
 	baseSubdomain := "_adscert." + currentDomainInfo.Domain
-	baseSubdomainRecords, err := cm.dnsResolver.LookupTXT(ctx, baseSubdomain)
+	baseSubdomainRecords, err := di.dnsResolver.LookupTXT(ctx, baseSubdomain)
 
 	if err != nil {
 		logger.Warningf("Error looking up record for %s in %v: %v", baseSubdomainRecords, time.Since(startTime), err)
@@ -155,11 +162,11 @@ func (cm *defaultDomainIndexer) checkDomainForPolicyRecords(ctx context.Context,
 	currentDomainInfo.lastUpdateTime = time.Now()
 }
 
-func (cm *defaultDomainIndexer) checkDomainForKeyRecords(ctx context.Context, currentDomainInfo *DomainInfo) {
+func (di *defaultDomainIndexer) checkDomainForKeyRecords(ctx context.Context, currentDomainInfo *DomainInfo) {
 
 	startTime := time.Now()
 	deliverySubdomain := "_delivery._adscert." + currentDomainInfo.Domain
-	deliverySubdomainRecords, err := cm.dnsResolver.LookupTXT(ctx, deliverySubdomain)
+	deliverySubdomainRecords, err := di.dnsResolver.LookupTXT(ctx, deliverySubdomain)
 
 	if err != nil {
 		logger.Warningf("Error looking up record for %s in %v: %v", deliverySubdomain, time.Since(startTime), err)
@@ -185,7 +192,7 @@ func (cm *defaultDomainIndexer) checkDomainForKeyRecords(ctx context.Context, cu
 	}
 
 	// create shared secrets for each private key + public key combination
-	for _, myKey := range cm.myPrivateKeys {
+	for _, myKey := range di.myPrivateKeys {
 		for _, theirKey := range currentDomainInfo.allPublicKeys {
 			keyPairAlias := newKeyPairAlias(myKey.alias, theirKey.alias)
 			if currentDomainInfo.allSharedSecrets[keyPairAlias] == nil {
@@ -197,18 +204,18 @@ func (cm *defaultDomainIndexer) checkDomainForKeyRecords(ctx context.Context, cu
 		}
 	}
 
-	currentDomainInfo.currentSharedSecretId = newKeyPairAlias(cm.currentPrivateKey, currentDomainInfo.currentPublicKeyId)
+	currentDomainInfo.currentSharedSecretId = newKeyPairAlias(di.currentPrivateKey, currentDomainInfo.currentPublicKeyId)
 	currentDomainInfo.lastUpdateTime = time.Now()
 }
 
-func (cm *defaultDomainIndexer) StopAutoUpdate() {
-	cm.ticker.Stop()
-	cm.cancel()
+func (di *defaultDomainIndexer) StopAutoUpdate() {
+	di.ticker.Stop()
+	di.cancel()
 }
 
-func (cm *defaultDomainIndexer) UpdateNow() {
+func (di *defaultDomainIndexer) UpdateNow() {
 	select {
-	case cm.wakeUp <- struct{}{}:
+	case di.wakeUp <- struct{}{}:
 		logger.Info("Wrote to wake-up channel.")
 		// Channel publish succeeded.
 	default:
