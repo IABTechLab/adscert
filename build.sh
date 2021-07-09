bc6635be60ec5f857bc28a82f3ad119a1ac9940d
Bret Ikehara <bret.k.ikehara@gmail.com>
2021-07-09 01:05:15 -0700

    generate protobuf files in standardized build env

--- build.sh
+++
@@ -1,8 +0,0 @@
-# generate protobuf/grpc code
-protoc --go_out=. --go_opt=module=github.com/IABTechLab/adscert --go-grpc_out=. --go-grpc_opt=module=github.com/IABTechLab/adscert ./api/adscert.proto
-
-# build grpc/server app
-go build ./cmd/server
-
-# build docker container
-### docker build -t adscert:latest .
