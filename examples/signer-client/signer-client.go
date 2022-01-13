d23945876577e6c7a15a1b0602fa940e740ccd6d
Curtis Light <curtisl@google.com>
2022-01-13 22:49:07 +0000

    Move health check code into its own function

--- examples/signer-client/signer-client.go
+++ examples/signer-client/signer-client.go
@@ -33,18 +33,9 @@ func main() {
 	}
 	defer conn.Close()
 
-	hctx, hcancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
-	defer hcancel()
-	healthClient := grpc_health_v1.NewHealthClient(conn)
-	healthCheckResponse, err := healthClient.Check(hctx, &grpc_health_v1.HealthCheckRequest{})
-	if err != nil {
-		log.Fatalf("Failed to pass heath check: %v", err)
-		return
-	}
-	if healthCheckResponse.Status != grpc_health_v1.HealthCheckResponse_SERVING {
-		log.Fatalf("Failed to pass heath status: %v", healthCheckResponse.Status)
-		return
-	}
+	// Optional: performs a health check against the server before actually
+	// trying to invoke the signatory service.
+	performHealthCheck(conn)
 
 	clientOpts := &signatory.AuthenticatedConnectionsSignatoryClientOptions{Timeout: 3 * time.Second}
 	signatoryClient := signatory.NewAuthenticatedConnectionsSignatoryClient(conn, clientOpts)
@@ -65,8 +56,21 @@ func main() {
 	}
 
 	if signatureResponse != nil && signatureResponse.RequestInfo != nil {
-		logger.Infof("signature response: %s", prototext.Format(signatureResponse))
+		logger.Infof("signature response:\n%s", prototext.Format(signatureResponse))
 	} else {
 		logger.Warningf("signature response is missing")
+	}
+}
+
+func performHealthCheck(conn *grpc.ClientConn) {
+	hctx, hcancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
+	defer hcancel()
+	healthClient := grpc_health_v1.NewHealthClient(conn)
+	healthCheckResponse, err := healthClient.Check(hctx, &grpc_health_v1.HealthCheckRequest{})
+	if err != nil {
+		log.Fatalf("Failed to pass heath check: %v", err)
+	}
+	if healthCheckResponse.Status != grpc_health_v1.HealthCheckResponse_SERVING {
+		log.Fatalf("Failed to pass heath status: %v", healthCheckResponse.Status)
 	}
 }
