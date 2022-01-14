7c8dd920b3636a744e23c8416b94f0b728816491
Curtis Light <curtisl@google.com>
2022-01-14 00:09:00 +0000

    Add more comments to the example signatory client code.

--- examples/signer-client/signer-client.go
+++ examples/signer-client/signer-client.go
@@ -25,8 +25,9 @@ var (
 func main() {
 	flag.Parse()
 
-	// create grpc connection for client to use
-	// options here use insecure defaults
+	// Establish the gRPC connection that the client will use to connect to the
+	// signatory server.  This basic example uses unauthenticated connections
+	// which should not be used in a production environment.
 	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
 	conn, err := grpc.Dial(*serverAddress, opts...)
 	if err != nil {
@@ -38,12 +39,19 @@ func main() {
 	// trying to invoke the signatory service.
 	performOptionalHealthCheckRPC(conn)
 
+	// Create a reusable Signatory Client that provides a lightweight wrapper
+	// around the RPC client stub.  This code performs some basic request
+	// timeout and error handling logic.
 	clientOpts := &signatory.AuthenticatedConnectionsSignatoryClientOptions{Timeout: *signingTimeout}
 	signatoryClient := signatory.NewAuthenticatedConnectionsSignatoryClient(conn, clientOpts)
 
+	// The RequestInfo proto contains details about the individual ad request
+	// being signed.  A SetRequestInfo helper function derives a hash of the
+	// destination URL and body, setting these value on the RequestInfo message.
 	reqInfo := &api.RequestInfo{}
 	signatory.SetRequestInfo(reqInfo, *destinationURL, []byte(*body))
 
+	// Request the signature.
 	logger.Infof("signing request for url: %v", *destinationURL)
 	signatureResponse, err := signatoryClient.SignAuthenticatedConnection(
 		&api.AuthenticatedConnectionSignatureRequest{
@@ -53,6 +61,8 @@ func main() {
 		logger.Warningf("unable to sign message: %v", err)
 	}
 
+	// In most circumstances a signatureResponse will be returned which includes
+	// detals about the successful or failed signature attempt.
 	if signatureResponse != nil {
 		logger.Infof("signature response:\n%s", prototext.Format(signatureResponse))
 	} else {
