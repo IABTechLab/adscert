71c5fdaf5f4ca2645a545f5289f5774c5bdcada0
Curtis Light <curtisl@google.com>
2022-01-21 18:55:43 +0000

    Move AdsCertSignatoryServer into a library package from main()

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"context"
 	crypto_rand "crypto/rand"
-	"errors"
 	"flag"
 	"fmt"
 	"net"
@@ -15,6 +13,7 @@ import (
 	"github.com/IABTechLab/adscert/pkg/adscert/discovery"
 	"github.com/IABTechLab/adscert/pkg/adscert/logger"
 	"github.com/IABTechLab/adscert/pkg/adscert/metrics"
+	"github.com/IABTechLab/adscert/pkg/adscert/server"
 	"github.com/IABTechLab/adscert/pkg/adscert/signatory"
 	"github.com/benbjohnson/clock"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -31,7 +30,6 @@ var (
 	domainCheckInterval   = flag.Duration("domain_check_interval", time.Duration(utils.GetEnvVarInt("DOMAIN_CHECK_INTERVAL", 30))*time.Second, "interval for checking domain records")
 	domainRenewalInterval = flag.Duration("domain_renewal_interval", time.Duration(utils.GetEnvVarInt("DOMAIN_RENEWAL_INTERVAL", 300))*time.Second, "interval before considering domain records for renewal")
 	privateKey            = flag.String("private_key", utils.GetEnvVarString("PRIVATE_KEY", ""), "base-64 encoded private key")
-	signatoryApi          *signatory.LocalAuthenticatedConnectionsSignatory
 )
 
 func main() {
@@ -47,7 +45,7 @@ func main() {
 		logger.Fatalf("Private key is required")
 	}
 
-	signatoryApi = signatory.NewLocalAuthenticatedConnectionsSignatory(
+	signatoryApi := signatory.NewLocalAuthenticatedConnectionsSignatory(
 		*origin,
 		crypto_rand.Reader,
 		clock.New(),
@@ -58,14 +56,17 @@ func main() {
 		[]string{*privateKey})
 
 	grpcServer := grpc.NewServer()
-	api.RegisterAdsCertSignatoryServer(grpcServer, &adsCertSignatoryServer{})
+	handler := &server.AdsCertSignatoryServer{
+		SignatoryAPI: signatoryApi,
+	}
+	api.RegisterAdsCertSignatoryServer(grpcServer, handler)
+	grpc_health_v1.RegisterHealthServer(grpcServer, handler)
+	reflection.Register(grpcServer)
+
 	logger.Infof("Starting AdsCert API server")
 	logger.Infof("Origin ads.cert Call Sign domain: %v", *origin)
 	logger.Infof("Port: %v", *serverPort)
 	logger.Infof("Log Level: %v", *logLevel)
-
-	grpc_health_v1.RegisterHealthServer(grpcServer, &adsCertSignatoryServer{})
-	reflection.Register(grpcServer)
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *serverPort))
 	if err != nil {
@@ -84,50 +85,4 @@ func runServer(l net.Listener, s *grpc.Server) {
 	if err != nil {
 		logger.Fatalf("Failed to serve GRPC: %v", err)
 	}
-}
-
-type adsCertSignatoryServer struct {
-	api.UnimplementedAdsCertSignatoryServer
-}
-
-func (s *adsCertSignatoryServer) SignAuthenticatedConnection(ctx context.Context, req *api.AuthenticatedConnectionSignatureRequest) (*api.AuthenticatedConnectionSignatureResponse, error) {
-	response, err := signatoryApi.SignAuthenticatedConnection(req)
-	return response, err
-}
-
-func (s *adsCertSignatoryServer) VerifyAuthenticatedConnection(ctx context.Context, req *api.AuthenticatedConnectionVerificationRequest) (*api.AuthenticatedConnectionVerificationResponse, error) {
-	response, err := signatoryApi.VerifyAuthenticatedConnection(req)
-	return response, err
-}
-
-func (s *adsCertSignatoryServer) Check(ctx context.Context, in *grpc_health_v1.HealthCheckRequest) (*grpc_health_v1.HealthCheckResponse, error) {
-	logger.Info("received health check request")
-	if signatoryApi == nil {
-		return &grpc_health_v1.HealthCheckResponse{
-			Status: grpc_health_v1.HealthCheckResponse_SERVICE_UNKNOWN,
-		}, errors.New("signatoryApi not initialized")
-	}
-	status := grpc_health_v1.HealthCheckResponse_SERVING
-	if !signatoryApi.IsHealthy() {
-		status = grpc_health_v1.HealthCheckResponse_NOT_SERVING
-	}
-	return &grpc_health_v1.HealthCheckResponse{
-		Status: status,
-	}, nil
-}
-
-func (s *adsCertSignatoryServer) Watch(r *grpc_health_v1.HealthCheckRequest, ws grpc_health_v1.Health_WatchServer) error {
-	logger.Info("received health check request")
-	if signatoryApi == nil {
-		return ws.Send(&grpc_health_v1.HealthCheckResponse{
-			Status: grpc_health_v1.HealthCheckResponse_SERVICE_UNKNOWN,
-		})
-	}
-	status := grpc_health_v1.HealthCheckResponse_SERVING
-	if !signatoryApi.IsHealthy() {
-		status = grpc_health_v1.HealthCheckResponse_NOT_SERVING
-	}
-	return ws.Send(&grpc_health_v1.HealthCheckResponse{
-		Status: status,
-	})
 }
