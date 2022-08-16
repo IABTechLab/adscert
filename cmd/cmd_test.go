9b93f23db7eb53b3858045ca29f2908e168c190d
HarrisonPearl <hspearl@gmail.com>
2022-08-16 23:45:33 +0000

    updates signature message

--- cmd/cmd_test.go
+++ cmd/cmd_test.go
@@ -34,8 +34,8 @@ func TestVerificationRequest(t *testing.T) {
 	testverifyParams.destinationURL = "https://adscerttestverifier.dev"
 	testverifyParams.serverAddress = "localhost:4000"
 	testverifyParams.body = ""
-	testverifyParams.verifyingTimeout = 5 * time.Millisecond
-	testverifyParams.signatureMessage = "from=adscerttestsigner.dev&from_key=LxqTmA&invoking=adscerttestverifier.dev&nonce=WjDf1dKgdvW1&status=1&timestamp=220816T200636&to=adscerttestverifier.dev&to_key=uNzTFA; sigb=UtzmQKT1M-4P&sigu=TUjCU24KC6Xz"
+	testverifyParams.verifyingTimeout = 20 * time.Millisecond
+	testverifyParams.signatureMessage = "from=adscerttestsigner.dev&from_key=LxqTmA&invoking=adscerttestverifier.dev&nonce=jsLwC53YySqG&status=1&timestamp=220816T221250&to=adscerttestverifier.dev&to_key=uNzTFA; sigb=NfCC9zQeS3og&sigu=1tkmSdEe-5D7"
 	if verifyRequest(testverifyParams).GetVerificationOperationStatus() != api.VerificationOperationStatus_VERIFICATION_OPERATION_STATUS_OK {
 		t.Fail()
 	}
