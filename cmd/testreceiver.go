a9332e7a9bede2f6e75e196c4c54ef4e36f0ee9e
HarrisonPearl <hspearl@gmail.com>
2022-08-22 21:05:33 +0000

    removes redundant writes

--- cmd/testreceiver.go
+++ cmd/testreceiver.go
@@ -16,50 +16,32 @@ limitations under the License.
 package cmd
 
 import (
-	"io/ioutil"
-	"time"
-
-	"github.com/spf13/cobra"
-	"google.golang.org/protobuf/encoding/prototext"
-
-	// "time"
-	// "github.com/IABTechLab/adscert/pkg/adscert/api"
-	// "github.com/IABTechLab/adscert/pkg/adscert/logger"
-	// "github.com/IABTechLab/adscert/pkg/adscert/signatory"
-	// "google.golang.org/grpc"
-	// "google.golang.org/grpc/credentials/insecure"
-	// "google.golang.org/protobuf/encoding/prototext"
-
 	"fmt"
+	"github.com/spf13/cobra"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
 
 // testreceiverCmd represents the test receivergit command
 var (
-	testreceiverParams = &testreceiverParameters{}
-
 	testreceiverCmd = &cobra.Command{
 		Use:   "testreceiver",
 		Short: "Run a simple local web server that will receive and verify requests",
 		Run: func(cmd *cobra.Command, args []string) {
-			startServer(testreceiverParams)
+			startServer()
 		},
 	}
 )
-
-type testreceiverParameters struct {
-}
 
 func init() {
 	rootCmd.AddCommand(testreceiverCmd)
 }
 
-func startServer(testreceiverParams *testreceiverParameters) {
-
+func startServer() {
 	// API routes
 	http.HandleFunc("/", func(w http.ResponseWriter, resp *http.Request) {
-		// fmt.Fprintf(w, "Hello world from adscert")
 		defer resp.Body.Close()
 
 		bodyBytes, err := ioutil.ReadAll(resp.Body)
@@ -70,34 +52,17 @@ func startServer(testreceiverParams *testreceiverParameters) {
 		bodyString := string(bodyBytes)
 		signatureMessage := resp.Header["X-Ads-Cert-Auth"][0]
 
-		// Print Statements
-		// Note if you try to access a key in the map that doesn't exist you will get an error
-		// a check on the key should be made to prevent this
-		// this code isn't meant for production
-		fmt.Fprint(w, "\n")
-		fmt.Fprint(w, signatureMessage)
-		fmt.Fprint(w, "\n")
-		fmt.Fprint(w, "\n")
-		fmt.Fprint(w, bodyString)
-		fmt.Fprint(w, "\n")
-		fmt.Fprint(w, "\n")
-
 		testverifyParams = &testverifyParameters{}
 		testverifyParams.destinationURL = bodyString
 		testverifyParams.signatureMessage = signatureMessage
 		testverifyParams.serverAddress = "localhost:3000"
 		testverifyParams.body = ""
 		testverifyParams.verifyingTimeout = 5 * time.Millisecond
-		fmt.Fprint(w, prototext.Format(verifyRequest(testverifyParams)))
-	})
-
-	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi")
+		verifyRequest(testverifyParams)
 	})
 
 	port := ":5000"
-	fmt.Println("Server is running on port" + port)
+	fmt.Println("Server running on port" + port)
 
-	// Start server on port specified above
 	log.Fatal(http.ListenAndServe(port, nil))
 }
