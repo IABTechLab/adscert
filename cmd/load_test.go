81e05fa038577a8752f6049502822be2e6d22fd0
HarrisonPearl <hspearl@gmail.com>
2022-10-11 15:22:22 +0000

    adds plotting

--- cmd/load_test.go
+++ cmd/load_test.go
@@ -4,29 +4,91 @@ import (
 	// "net/http"
 	"fmt"
 	"github.com/IABTechLab/adscert/pkg/adscert/api"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/plotutil"
+	"gonum.org/v1/plot/vg"
 	"testing"
 	"time"
 )
 
-func TestLoad10To1000SigningRequest(t *testing.T) {
+func TestLoadSigningRequest(t *testing.T) {
 	testsignParams := &testsignParameters{}
 	testsignParams.url = "https://adscerttestverifier.dev"
 	testsignParams.serverAddress = "localhost:3000"
 	testsignParams.body = ""
 	testsignParams.signingTimeout = 1 * time.Second
 
+	testsPerTestSize := 10
 	c := make(chan api.SignatureOperationStatus)
-	iterationResults := map[int][]int{}
-	testsPerSize := 10
+	iterationResults := map[int][]float64{}
 	for numOfRequests := 10; numOfRequests <= 10000; numOfRequests *= 10 {
-		for i := 0; i < testsPerSize; i++ {
+		for i := 0; i < testsPerTestSize; i++ {
 			iterationResult := sendSignatureRequests(numOfRequests, testsignParams, c)
-			iterationResults[iterationResult[0]] = append(iterationResults[iterationResult[0]], iterationResult[1])
+			iterationResults[iterationResult[0]] = append(iterationResults[iterationResult[0]], float64(iterationResult[1]))
 		}
 	}
 
 	for key, value := range iterationResults {
 		fmt.Printf("%v Signing Attempts: %v succeeded\n", key, value)
+	}
+	plotResults(iterationResults)
+}
+
+func plotResults(iterationResults map[int][]float64) {
+	groupA := append(plotter.Values{}, iterationResults[10]...)
+	groupB := append(plotter.Values{}, iterationResults[100]...)
+	groupC := append(plotter.Values{}, iterationResults[1000]...)
+	groupD := append(plotter.Values{}, iterationResults[10000]...)
+
+	p := plot.New()
+
+	p.Title.Text = "Bar chart"
+	p.Y.Label.Text = "Heights"
+
+	w := vg.Points(20)
+
+	barsA, err := plotter.NewBarChart(groupA, w)
+	if err != nil {
+		panic(err)
+	}
+	barsA.LineStyle.Width = vg.Length(0)
+	barsA.Color = plotutil.Color(0)
+	barsA.Offset = -w
+
+	barsB, err := plotter.NewBarChart(groupB, w)
+	if err != nil {
+		panic(err)
+	}
+	barsB.LineStyle.Width = vg.Length(0)
+	barsB.Color = plotutil.Color(1)
+
+	barsC, err := plotter.NewBarChart(groupC, w)
+	if err != nil {
+		panic(err)
+	}
+	barsC.LineStyle.Width = vg.Length(0)
+	barsC.Color = plotutil.Color(2)
+	barsC.Offset = w
+
+	barsD, err := plotter.NewBarChart(groupD, w)
+	if err != nil {
+		panic(err)
+	}
+	barsD.LineStyle.Width = vg.Length(0)
+	barsD.Color = plotutil.Color(2)
+	barsD.Offset = w
+
+	p.Add(barsA, barsB, barsC, barsC)
+	p.Legend.Add("10", barsA)
+	p.Legend.Add("100", barsB)
+	p.Legend.Add("1000", barsC)
+	p.Legend.Add("10000", barsD)
+	p.Legend.Top = true
+	p.NominalX("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")
+
+	if err := p.Save(5*vg.Inch, 3*vg.Inch, "barchart.png"); err != nil {
+		panic(err)
 	}
 }
 
