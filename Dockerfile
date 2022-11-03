069dff40449ba8387e165ede8a440656720eadd1
HarrisonPearl <31372465+HarrisonPearl@users.noreply.github.com>
2022-11-03 13:48:57 -0400

    removes usage of grpc health check (#88)

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,6 @@
 FROM golang:1.17 as builder
 COPY . /src
 WORKDIR /src
-RUN GRPC_HEALTH_PROBE_VERSION=v0.3.1 && \
-    wget -qO/bin/grpc_health_probe https://github.com/grpc-ecosystem/grpc-health-probe/releases/download/${GRPC_HEALTH_PROBE_VERSION}/grpc_health_probe-linux-amd64 && \
-    chmod +x /bin/grpc_health_probe
 RUN go mod download
 RUN CGO_ENABLED=0 GOOS=linux go build .
 RUN CGO_ENABLED=0 GOOS=linux go build ./cmd/server
@@ -13,7 +10,6 @@ WORKDIR /app
 RUN apk --no-cache add ca-certificates
 COPY --from=builder /src/adscert .
 COPY --from=builder /src/server .
-COPY --from=builder /bin/grpc_health_probe /bin/grpc_health_probe
 
 ENV LOGLEVEL=""
 ENV ORIGIN=""
--- pkg/adscert/server/adscert_signatory_server.go
+++ pkg/adscert/server/adscert_signatory_server.go
@@ -2,12 +2,9 @@ package server
 
 import (
 	"context"
-	"errors"
 
 	"github.com/IABTechLab/adscert/pkg/adscert/api"
-	"github.com/IABTechLab/adscert/pkg/adscert/logger"
 	"github.com/IABTechLab/adscert/pkg/adscert/signatory"
-	"google.golang.org/grpc/health/grpc_health_v1"
 )
 
 type AdsCertSignatoryServer struct {
@@ -24,36 +21,4 @@ func (s *AdsCertSignatoryServer) SignAuthenticatedConnection(ctx context.Context
 func (s *AdsCertSignatoryServer) VerifyAuthenticatedConnection(ctx context.Context, req *api.AuthenticatedConnectionVerificationRequest) (*api.AuthenticatedConnectionVerificationResponse, error) {
 	response, err := s.SignatoryAPI.VerifyAuthenticatedConnection(req)
 	return response, err
-}
-
-func (s *AdsCertSignatoryServer) Check(ctx context.Context, in *grpc_health_v1.HealthCheckRequest) (*grpc_health_v1.HealthCheckResponse, error) {
-	logger.Info("received health check request")
-	if s.SignatoryAPI == nil {
-		return &grpc_health_v1.HealthCheckResponse{
-			Status: grpc_health_v1.HealthCheckResponse_SERVICE_UNKNOWN,
-		}, errors.New("signatoryApi not initialized")
-	}
-	status := grpc_health_v1.HealthCheckResponse_SERVING
-	if !s.SignatoryAPI.IsHealthy() {
-		status = grpc_health_v1.HealthCheckResponse_NOT_SERVING
-	}
-	return &grpc_health_v1.HealthCheckResponse{
-		Status: status,
-	}, nil
-}
-
-func (s *AdsCertSignatoryServer) Watch(r *grpc_health_v1.HealthCheckRequest, ws grpc_health_v1.Health_WatchServer) error {
-	logger.Info("received health check request")
-	if s.SignatoryAPI == nil {
-		return ws.Send(&grpc_health_v1.HealthCheckResponse{
-			Status: grpc_health_v1.HealthCheckResponse_SERVICE_UNKNOWN,
-		})
-	}
-	status := grpc_health_v1.HealthCheckResponse_SERVING
-	if !s.SignatoryAPI.IsHealthy() {
-		status = grpc_health_v1.HealthCheckResponse_NOT_SERVING
-	}
-	return ws.Send(&grpc_health_v1.HealthCheckResponse{
-		Status: status,
-	})
 }
